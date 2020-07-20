1b11ad5ca67d8392ccb72a5ed0b9d0b6032f09cf
Hengda Shi <hengda.shi@engineering.ucla.edu>
2020-07-20 16:58:46 -0700

    added install script

--- install.sh
+++ install.sh
@@ -1,6 +1,37 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-# os initialization script
+# os initialization script (macos->Darwin, linux->Linux)
+UNAME=$(uname)
+DOTPATH=$(dirname $(realpath "$0"))
 
+if [[ ! -d ~/.zprezto/ ]]; then
+    # switch to zsh & prezto
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+fi
+
+# switch default shell to zsh
+chsh -s /bin/zsh
+
+# configure git
+git config --global user.name "Hengda Shi"
+git config --global user.email "hengda.shi@engineering.ucla.edu"
+git config --global core.editor "vim"
+
+if [[ -d ~/.config/nvim ]]; then
+    ln -fs ${DOTPATH}/vim/init.vim ~/.config/nvim/init.vim
+    rm -r ~/.config/nvim/config
+    mkdir ~/.config/nvim/config
+    for vimrc in ${DOTPATH}/vim/config/*; do
+        ln -fs ${vimrc} ~/.config/nvim/config/$(basename ${vimrc})
+    done
+fi
+
+if [[ ! -e ~/.tmux.conf ]]; then
+    ln -fs ${DOTPATH}/tmux/.tmux.conf ~/.tmux.conf
+fi
 
