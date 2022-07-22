28e0720a169470354758666d4b71404427ca3c2d
Hengda Shi <hengda.shi@cs.ucla.edu>
2022-07-22 14:50:36 -0700

    remove nvchad submodule from dotfile

--- vim/install.sh
+++ vim/install.sh
@@ -11,30 +11,23 @@ CWD=$(cd $(dirname "$0") && pwd -P)
 CONFIG="$1"
 
 
-# install neovim dependencies
-pip install neovim
-if ! command -v yarn &> /dev/null; then
-  npm install -g neovim
-else
-  yarn global add neovim 
-fi
-
 # create dirs
 [[ -d ~/.config/nvim ]] && rm -r ~/.config/nvim
 
 # link all neovim config
 if [[ "${CONFIG}" == "lua" ]]; then
-  mkdir -p ~/.config/nvim/lua/custom
-  # install nvchad configs
-  ln -fs ${CWD}/lua/nvchad/init.lua ~/.config/nvim/init.lua
-  for conf in ${CWD}/lua/nvchad/lua/*; do
-    ln -fs ${conf} ~/.config/nvim/lua/$(basename ${conf})
-  done
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
   # install custom configs
-  for conf in ${CWD}/lua/custom/*; do
-    ln -fs ${conf} ~/.config/nvim/lua/custom/$(basename ${conf})
-  done
+  ln -fs ${CWD}/lua/custom/ ~/.config/nvim/
 else
+  # install neovim dependencies
+  pip install neovim
+  if ! command -v yarn &> /dev/null; then
+    npm install -g neovim
+  else
+    yarn global add neovim 
+  fi
+
   mkdir -p ~/.config/nvim/config
   mkdir -p ~/.local/share/nvim/plugged
   mkdir -p ~/.vim/files/info
---
+++ vim/lua/custom/chadrc.lua
@@ -0,0 +1,11 @@
+-- Chadrc overrides 
+
+local M = {}
+
+M.ui = {
+  statusline = {
+    separator_style = "block",
+  },
+}
+
+return M
