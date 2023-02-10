53cae04918e345719117d31fbb92d6ce1cb5320d
Hengda Shi <hengda.shi@cs.ucla.edu>
2023-02-10 14:20:06 -0800

    fix(nvim, lazyvim): add custom comment key map

---
+++ vim/lazyvim/lua/plugins/_mini_comment.lua
@@ -0,0 +1,10 @@
+return {
+  'echasnovski/mini.comment',
+  opts = {
+    mappings = {
+      comment = '<leader>c<leader>',
+      comment_line = '<leader>c<leader>',
+      textobject = '<leader>c<leader>',
+    },
+  },
+}
