2f7b4f2ab13b5cbba14a05af6f549470415db6f1
Hengda Shi <hengda.shi@cs.ucla.edu>
2023-10-12 13:50:18 -0700

    misc(lazyvim): remove default configs in indent blankline

--- vim/lazyvim/lua/plugins/_indent_blankline.lua
+++ vim/lazyvim/lua/plugins/_indent_blankline.lua
@@ -1,19 +1,3 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
-  opts = {
-    filetype_exclude = {
-      'help',
-      'terminal',
-      'alpha',
-      'dashboard',
-      'neo-tree',
-      'lspinfo',
-      'TelescopePrompt',
-      'TelescopeResults',
-      'lsp-installer',
-      'norg',
-      'Trouble',
-      'lazy',
-    },
-  },
 }
