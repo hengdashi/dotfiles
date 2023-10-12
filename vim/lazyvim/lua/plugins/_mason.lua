ce8e65738356df6076d4b090aa3cf59b5eab0287
Hengda Shi <hengda.shi@cs.ucla.edu>
2023-10-12 14:28:14 -0700

    feat(lazyvim): install default lsp in mason

--- vim/lazyvim/lua/plugins/_mason.lua
+++ vim/lazyvim/lua/plugins/_mason.lua
@@ -1,8 +1,20 @@
 return {
   'williamboman/mason.nvim',
   opts = {
+    -- merging list is not possible, list must be overwritten
     ensure_installed = {
+      -- lsp
       'lua-language-server',
+      'json-lsp',
+      'ruff-lsp',
+      -- dap
+      -- linter
+      'buf',
+      'ruff',
+      -- formatter
+      'black',
+      'stylua',
+      'shfmt',
     },
   },
 }
