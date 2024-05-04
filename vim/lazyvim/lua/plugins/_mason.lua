3c62fd5825a2318c8dd77886d6cb1f89be051c04
Hengda Shi <hengda.shi@cs.ucla.edu>
2024-05-04 10:19:00 -0700

    fix(lazyvim): autoformat

--- vim/lazyvim/lua/plugins/_mason.lua
+++ vim/lazyvim/lua/plugins/_mason.lua
@@ -12,7 +12,6 @@ return {
       "buf",
       "ruff",
       -- formatter
-      "black",
       "stylua",
     },
   },
--- vim/lazyvim/lua/plugins/_persisted.lua
+++ vim/lazyvim/lua/plugins/_persisted.lua
@@ -1,14 +1,16 @@
 return {
-  'olimorris/persisted.nvim',
+  "olimorris/persisted.nvim",
   opts = {
     use_git_branch = true,
     should_autosave = function()
-      if vim.bo.filetype == "alpha" then return false end
+      if vim.bo.filetype == "alpha" then
+        return false
+      end
       return true
     end,
   },
   config = function(_, opts)
-    require('persisted').setup(opts)
-    require("telescope").load_extension("persisted")   -- To load the telescope extension
+    require("persisted").setup(opts)
+    require("telescope").load_extension("persisted") -- To load the telescope extension
   end,
 }
