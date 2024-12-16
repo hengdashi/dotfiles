33dcee8cb51a23baef3336194b5d7762041b4881
Hengda Shi <hengda.shi@cs.ucla.edu>
2024-12-15 20:35:53 -0800

    fix(lazyvim): remove remaining persisted mentions and enable telescope

--- vim/lazyvim/lua/plugins/_telescope.lua
+++ vim/lazyvim/lua/plugins/_telescope.lua
@@ -1,12 +1,11 @@
 return {
   'nvim-telescope/telescope.nvim',
   keys = {
-    { '<leader>fp', '<cmd> Telescope persisted <CR>', desc = 'Find Projects' },
   },
   opts = {
     defaults = {
       prompt_prefix = '  ',
     },
-    extensions_list = { 'themes', 'terms', 'persisted', 'fzf' },
+    extensions_list = { 'themes', 'terms', 'fzf' },
   },
 }
